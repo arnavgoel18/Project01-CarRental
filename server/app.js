81f5de90b06a2552513c8b1a945a176d5667b641
salonivij14 <salonivij14@gmail.com>
2021-07-16 17:35:40 +0400

    file structure changed

--- server/app.js
+++ server/app.js
@@ -2,11 +2,9 @@ const express= require('express'); //install express
 const mysql= require('mysql'); //install mysql
 const dotenv=require('dotenv'); //install dotenv
 const cors=require('cors'); //install cors
-const bcrypt=require('bcryptjs')//install bcrypt
 
 const perm=require('./perm'); //get the perm middleware
 
-const saltRounds=10
 
 const app=express(); // start server
 app.use(cors({ credentials: true })) //setup cors
@@ -29,67 +27,9 @@ db.connect((error) => { //to actually connect, if you have made any error above,
         console.log("Connected to MYSQL")
     } 
 })
-app.post('/register', (req,res)=>{
-    const {firstName, lastName, age, userNameReg, passwordReg}=req.body
 
-    bcrypt.hash(passwordReg,saltRounds, (err,hash)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            db.query('INSERT INTO account_info SET ?', {
-                first_name: firstName, 
-                last_name:lastName, 
-                age:age,
-                userid: userNameReg, 
-                password: hash
-            }, (error, results)=>{
-                if(error){
-                    console.log(error);
-                }
-                else{
-                    console.log(results);
-                    return res.render('register', {
-                        message: 'User registered'
-                    });
-                }
-            })
-        }
-    })
-})
+app.use('/', require('./routes/auth'));
 
-app.post('/login', (req,res)=>{
-    const userid=req.body.userid
-    const password=req.body.password
-
-    if(!userid || !password){
-        return res.status(400).render('login', {
-            message: 'Please provide the details'
-        })
-    }
-    
-    db.query('SELECT * FROM account_info WHERE userid=?',[userid],
-     async (error, results)=>{
-        if(error){
-            res.send({error:error})
-        }
-        else{
-            if(results.length>0){
-                bcrypt.compare(password, result[0].password, (err, response)=>{
-                    if(response){
-                        res.send(results)
-                    }
-                    else{
-                        res.send({message: 'Wrong credentials'})
-                    }
-                })
-            }
-            else{
-                res.send({message: 'User does not exist'})
-            }
-            }
-        })
-    })
 
 app.listen("3001", ()=>{
     console.log("Server started on port 3001");
