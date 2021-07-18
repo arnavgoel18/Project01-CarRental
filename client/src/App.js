81203bf4f0566081638ae8cd75313f8d7953df34
arnavgoel18 <arnav18.goel@gmail.com>
2021-07-18 14:10:51 +0530

    Card Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import BookCard from './components/BookCard/BookCard';
 
 function App() {
   return (
     <>
-      
+      <BookCard/>
     </>
   );
 }
---
+++ client/src/components/BookCard/BookCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import './BookCard.css'
+
+import pic from '../../assets/Scorpio.png'
+
+// icons imports
+import { MdAirlineSeatReclineExtra, MdAllInclusive, MdPersonAdd } from 'react-icons/md'
+import { FaTachometerAlt, FaGasPump, FaCar } from 'react-icons/fa'
+import { GiJoystick } from 'react-icons/gi'
+import { VscSymbolColor } from 'react-icons/vsc'
+
+//data import
+
+
+function BookCard() {
+    return (
+        <React.Fragment>
+            <div className="bookCard-container">
+                <div className="col1">
+                    <img src={pic} alt=""/>
+                    <ul>
+                        <li id="color-model"> Mahindra Scorpio</li>
+                    </ul>
+                </div>
+                <div className="col2 cols">
+                    <ul>
+                        <li><MdAirlineSeatReclineExtra/> 5 Seater</li>
+                        <li><FaTachometerAlt/> 25 km/L</li>
+                        <li><MdAllInclusive/> Gas Charges Inc.</li>
+                        <li><FaGasPump/> Diesel</li>
+                    </ul>
+                </div>
+                <div className="col3 cols">
+                    <ul>
+                        <li><FaCar/> Sedan</li>
+                        <li><GiJoystick/> Automatic </li>
+                        <li><VscSymbolColor/> Red</li>
+                        <li><MdPersonAdd/> 18+</li>
+                    </ul>
+                </div>
+                <div className="col4 cols">
+                    <ul>
+                        <li> $5000 / Day </li>
+                        <li> <button className="btn">Book Now</button> </li>
+                        <li className="terms-and-cond"> <a href="">T&C</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default BookCard
