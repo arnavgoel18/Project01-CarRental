99ef2be4eeb4d166ebc0d95e29ede8287de66c6f
arnavgoel18 <arnav18.goel@gmail.com>
2021-07-19 15:21:45 +0530

    Minor Changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-// import ContactUs from './containers/ContactUs/ContactUs'
-// import Navbar from './components/Navbar/Navbar'
-// import Footer from './components/Footer/Footer'
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
-// function Page(){
-//     return(
-//     <>
-       
-//     </>
-//     );
-// }
-
-// ReactDOM.render(<Page/>, document.getElementById('root'));
 
