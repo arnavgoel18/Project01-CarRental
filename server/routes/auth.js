182cbbf19839b6f0d8f33d1d0c88414a3b7221ea
salonivij14 <salonivij14@gmail.com>
2021-07-21 17:09:26 +0400

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,6 @@ const router= express.Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
-router.get('/auth', validateToken, (req, res) => {
-    res.send(req.user);
-  });
+router.get('/auth', validateToken);
 
 module.exports= router;
